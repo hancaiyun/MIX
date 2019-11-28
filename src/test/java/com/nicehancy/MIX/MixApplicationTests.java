48e2904901ce840dd5913ae1e93d06b5ae57395e
hancaiyun <hancaiyun-it@bestpay.com.cn>
2019-11-28 20:06:11 +0800

    first commit

---
+++ src/test/java/com/nicehancy/MIX/MixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicehancy.MIX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
