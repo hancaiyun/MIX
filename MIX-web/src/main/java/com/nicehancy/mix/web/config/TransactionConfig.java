d43cd3082811650ee914a90d31dcb85e77a6bddf
hancaiyun <nicehancy@yeah.net>
2020-05-18 14:23:24 +0800

    新增core层

---
+++ MIX-web/src/main/java/com/nicehancy/mix/web/config/TransactionConfig.java
@@ -0,0 +1,21 @@
+package com.nicehancy.mix.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+
+/**
+ * <p>
+ * <p/>
+ *
+ * @author hancaiyun
+ * @since 2020/5/18 14:10
+ **/
+@Configuration
+public class TransactionConfig {
+    @Bean
+    MongoTransactionManager transactionManager(MongoDbFactory factory){
+        return new MongoTransactionManager(factory);
+    }
+}
